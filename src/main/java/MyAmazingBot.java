04629b4ab3c043dff4b761a930be50f4aacd234d
Denys Babkin <Denys.Babkin@teaminternational.com>
2017-08-18 11:28:50 +0300

    Initial commit

---
+++ src/main/java/MyAmazingBot.java
@@ -0,0 +1,39 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            System.out.println(chat_id + " " + message_text);
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // Return bot username
+        // If bot username is @MyAmazingBot, it must return 'MyAmazingBot'
+        return "MyAmazingBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // Return bot token from BotFather
+        return "388216169:AAFaGwWn6Czhuud2jfdHqJrO6hwisRuGoOc";
+    }
+}
